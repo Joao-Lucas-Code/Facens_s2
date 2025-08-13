b6f279c4c460e31c38c850d8f401098a31e65084
João Lucas <joao.lgomes1166@gmail.com>
2025-08-13 18:13:16 -0300

    criando os arquivos html e css com o pyautogui para Web Design

--- pyautogui/run.py
+++ pyautogui/run.py
@@ -1,11 +1,18 @@
 import pyautogui
 from time import sleep
 cont = 0 # Inicio da contagem
-for c in range(10): # Tamanho da contagem
+for c in range(11): # Tamanho da contagem
     pyautogui.moveTo(x=267, y=109)
     sleep(0.4)
     pyautogui.click(x=267, y=109)
     sleep(0.5)
-    pyautogui.write(f"aula_{cont}.c")
+    pyautogui.write(f"aula_{cont}.html")
+    pyautogui.press("enter")
+
+    pyautogui.moveTo(x=267, y=109)
+    sleep(0.4)
+    pyautogui.click(x=267, y=109)
+    sleep(0.5)
+    pyautogui.write(f"aula_{cont}.css")
     pyautogui.press("enter")
     cont+=1
