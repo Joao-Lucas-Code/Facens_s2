0d7b2833939020fb5b2e5eb591c6fd75a75cf53f
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 19:51:39 -0300

    void aloca e recebe

---
+++ Programacao_Estruturada/C/aula_7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+void aloca_int(float **p, int tam);
+void recebe(float *p, int tam);
+int main(void)
+{
+  float *ptr = NULL;
+  aloca_int(&ptr, 10);
+  recebe(ptr, 10);
+
+  return 0;
+}
+void recebe(float *p, int tam)
+{
+  for (int i = 0; i < tam; i++)
+  {
+    printf("\nElemento [%d]: ", i+1);
+    scanf("%f", &p[i]);
+    fflush(stdin);
+  }
+}
+
+void aloca_int(float **p, int tam)
+{
+  if ((*p = (float *)realloc(*p, tam * sizeof(float))) == NULL)
+  {
+    printf("Erro na alocacao!");
+    exit(1);
+  }
+  printf("\nFuncao aloca - endereco = %p", *p);
+}
